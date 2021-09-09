2b46cb7d0d89dbb45a154b0bfc49aba2f028b697
Ryan Rennoir <ryanrennoir9@gmail.com>
2021-09-09 19:48:03 +0200

    Add possibility to run the server alone (headless)

--- Server.py
+++ Server.py
@@ -1,6 +1,8 @@
 import queue
 import socket
 import struct
+import sys
+import getopt
 import threading
 import time
 from dataclasses import dataclass
@@ -233,3 +235,54 @@ class ServerInstance:
 
         self._server_event.set()
         self._server_thread.join()
+
+
+def headless(argv: List[str]) -> None:
+
+    """
+    Ugly isn't it ?
+    """
+
+    try:
+        opts, args = getopt.getopt(argv[1:], "hp:", ["help", "port="])
+
+    except getopt.GetoptError as err:
+
+        print(err)
+        sys.exit(2)
+
+    port = 4269
+    for opt, arg in opts:
+
+        if opt in ("-h", "--help"):
+            print("Server.py [-p <port> (default 4269)]")
+            sys.exit()
+
+        elif opt in ("-p", "--port"):
+
+            if arg.isnumeric():
+                port = int(arg)
+
+            else:
+                print(f"Invalide port arg: {arg}")
+                sys.exit(1)
+
+    server = ServerInstance(port)
+    print("SERVER: Running as headless server")
+
+    Running = True
+    while Running:
+
+        try:
+            time.sleep(1)
+
+        except KeyboardInterrupt:
+            Running = False
+
+    server.disconnect()
+    print("SERVER: exiting")
+
+
+if __name__ == "__main__":
+
+    headless(sys.argv)
