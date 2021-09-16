d5f5f4ba5b7b401cb372f97a9181e7384945e0d9
Jan <34112129+itsmylife44@users.noreply.github.com>
2021-09-17 00:22:56 +0200

    Windows Build
    Create Windows Build without using Python.

---
+++ setup.py
@@ -0,0 +1,18 @@
+import sys
+from cx_Freeze import setup, Executable
+
+build_exe_options = {"packages": ["os"], 'include_files':['Config/','modules/','SharedMemory/','Assets/','headless_server.py']}
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+#setup
+setup(
+    name = "PyAccEngineer",
+    version = "1.4_beta",
+    description = "Acc pit Engineer",
+    author = "Ryan Rennoir",
+    options = {"build_exe": build_exe_options},
+    executables = [Executable("main.py", base=base, icon="Assets/Icon/techSupport.ico")]
+)
