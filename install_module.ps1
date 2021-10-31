f3cd36d0de9894393cada3a3f2247c11b4da32d8
Ryan Rennoir <32205591+rrennoir@users.noreply.github.com>
2021-10-31 16:41:27 +0100

    Improve scripts and use venv

--- install_module.ps1
+++ install_module.ps1
@@ -1,1 +1,14 @@
+Write-Output "Create venv"
+python -m venv .\PyAccEngineerEnv
+
+Write-Output "Activate venv"
+.\PyAccEngineerEnv\Scripts\activate
+
+Write-Output "Install required package"
 pip install -r requirement.txt
+
+Write-Host "Press any key to continue..."
+$Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+Write-Output "Exit Python venv"
+deactivate
--- start_PyAccEngineer.ps1
+++ start_PyAccEngineer.ps1
@@ -1,1 +1,11 @@
+Write-Host "Active venv"
+.\PyAccEngineerEnv\Scripts\activate
+
+Write-Host "Stating PyAccEngineer"
 python .\main.py
+
+Write-Host "Press any key to continue..."
+$Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+Write-Host "Exit Python venv"
+deactivate
