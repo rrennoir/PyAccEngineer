73beb4766bfffb37668591f6473c2a2538151800
Ryan Rennoir <32205591+rrennoir@users.noreply.github.com>
2023-03-10 22:39:11 +0100

    Create requirement.txt for Python 3.11

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO,
                     datefmt="%H:%M:%S")
 
 
-_VERSION_ = "1.5.9"
+_VERSION_ = "1.5.9b"
 
 
 class ConnectionPage(ttk.Frame):
